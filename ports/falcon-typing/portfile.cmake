8ab0a4dd9599eaa80da151db1dec6eec7fae13fd
tjk144 <tkovach@wisc.edu>
2026-04-29 15:49:21 -0500

    bumped versions for gcc build

--- ports/falcon-typing/portfile.cmake
+++ ports/falcon-typing/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO falcon-autotuning/falcon-typing
-    REF v1.0.0
-    SHA512 9be44e054f334acefe140d13d893694c9fd981e4b906c91b52370e5a35b3dcd045bdee851608918d60b2859438caaf397c12032b606d19f4e603dc7ea7e8481d
+    REF v${VERSION}
+    SHA512 b352e54aade4a23f081406c21a1b1272403efc2f999fe50f5064c2358e3a5c8e2ddbc2bb175cbdda9497092623efc272e806bbdfa16d3418f894b033c035b507
 )
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
